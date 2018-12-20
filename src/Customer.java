34a7364cfea47ff7ab2193e8e7d6d90324b24cf3
wyj5230 <e0000371@u.nus.edu>
2018-12-21 02:27:31 +0800

    impl logic

--- src/Customer.java
+++ src/Customer.java
@@ -14,4 +14,135 @@ public class Customer
     public String msgOn;
     public String createdTime;
 
+    public Customer(String userName, String taoBaoId, String city, String district, String address, String
+            informationSource, String emergencyContact, String beizhu, String msgOn, String createdTime)
+    {
+        this.userName = userName;
+        this.taoBaoId = taoBaoId;
+        this.city = city;
+        this.district = district;
+        this.address = address;
+        this.informationSource = informationSource;
+        this.emergencyContact = emergencyContact;
+        this.beizhu = beizhu;
+        this.msgOn = msgOn;
+        this.createdTime = createdTime;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getTaoBaoId()
+    {
+        return taoBaoId;
+    }
+
+    public void setTaoBaoId(String taoBaoId)
+    {
+        this.taoBaoId = taoBaoId;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getDistrict()
+    {
+        return district;
+    }
+
+    public void setDistrict(String district)
+    {
+        this.district = district;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getInformationSource()
+    {
+        return informationSource;
+    }
+
+    public void setInformationSource(String informationSource)
+    {
+        this.informationSource = informationSource;
+    }
+
+    public String getEmergencyContact()
+    {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact)
+    {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public String getBeizhu()
+    {
+        return beizhu;
+    }
+
+    public void setBeizhu(String beizhu)
+    {
+        this.beizhu = beizhu;
+    }
+
+    public String getMsgOn()
+    {
+        return msgOn;
+    }
+
+    public void setMsgOn(String msgOn)
+    {
+        this.msgOn = msgOn;
+    }
+
+    public String getCreatedTime()
+    {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime)
+    {
+        this.createdTime = createdTime;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Customer{" +
+                "userName='" + userName + '\'' +
+                ", taoBaoId='" + taoBaoId + '\'' +
+                ", city='" + city + '\'' +
+                ", district='" + district + '\'' +
+                ", address='" + address + '\'' +
+                ", informationSource='" + informationSource + '\'' +
+                ", emergencyContact='" + emergencyContact + '\'' +
+                ", beizhu='" + beizhu + '\'' +
+                ", msgOn='" + msgOn + '\'' +
+                ", createdTime='" + createdTime + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,12 @@
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-
-import org.apache.http.util.EntityUtils;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import javax.swing.filechooser.FileSystemView;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Future;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main
 {
@@ -29,10 +14,14 @@ public class Main
 
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        astronVote();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        String inputDate = reader.next();
+        reader.close();
+        astronVote(inputDate);
     }
 
-    private static void astronVote() throws IOException
+    private static void astronVote(String inputDate) throws IOException
     {
         String loginUrl = "https://xcx.iwalkpet.com/index.php/manage/lfjoew/login/vali";
         String userPageUrl = "";
@@ -44,114 +33,48 @@ public class Main
             userPageUrl = userManagementPanel.first().attr("href");
             System.out.println(userPageUrl);
         }
-
+        ArrayList<Customer> customers = new ArrayList<>();
         do
         {
+            //set value
             Document adminDoc = Jsoup.connect(userPageUrl).get();
             Elements adminElements = adminDoc.select("table.table tbody tr");
             for (Element e : adminElements)
             {
-
+                Elements customerInfo = e.select("td");
+                String dateString = customerInfo.get(0).text();
+                if (inputDate.compareToIgnoreCase(dateString.substring(0, 10)) <= 0)
+                {
+                    Customer customer = new Customer(customerInfo.get(0).text(), customerInfo.get(1).text(),
+                            customerInfo.get
+                            (2).text(), customerInfo.get(3).text(), customerInfo.get(4).text(), customerInfo.get(5).text
+                            (), customerInfo.get(6).text(), customerInfo.get(7).text(), customerInfo.get(8).text(),
+                            customerInfo.get(9).text());
+                    System.out.println(customer.toString());
+                    customers.add(customer);
+                }
+                else
+                {
+                    System.out.println("end");
+                    break;
+                }
             }
-        } while (true);
-
-//        HttpClient httpClient = HttpClientBuilder.create().build();
-//        HttpPost request = new HttpPost("http://www.stepon.top/misc/voicevote/api.php?op=vote");
-//        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(6000).setConnectTimeout(6000).build();
-//        request.setConfig(requestConfig);
-//        StringEntity params = new StringEntity("vote-id=3025&token=" + token,
-//                ContentType
-//                        .APPLICATION_FORM_URLENCODED);
-//        request.addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X)
-// AppleWebKit/537.36" +
-//                " (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36");
-//        request.addHeader("Origin", "http://www.stepon.top");
-//        request.addHeader("Host", "www.stepon.top");
-//        request.addHeader("Referer", "http://www.stepon.top/misc/voicevote/index" +
-//                ".php?from=groupmessage&isappinstalled=0");
-//        request.addHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-//        request.addHeader("Accept", "application/json, text/javascript, */*; q=0.01");
-//        request.addHeader("Accept-Encoding", "gzip, deflate");
-//        request.addHeader("Accept-Language", "zh-CN,zh;q=0.8");
-//        request.addHeader("Connection", "keep-alive");
-//        request.addHeader("Cookie", "PHPSESSID=" + phpsessid + ";
-// Hm_lvt_9045f321c9885a5d2142d00c75d836e2=1495892588;");
-//        request.addHeader("X-Requested-With", "XMLHttpRequest");
-//        request.setEntity(params);
-//
-//        try {
-//            HttpResponse response = httpClient.execute(request);
-//            HttpEntity entity = response.getEntity();
-//            String responseString = EntityUtils.toString(entity, "UTF-8");
-//            System.out.println(responseString);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+            //set next page
+            userPageUrl = null;
+            Elements pages = adminDoc.select("div.c-pager a");
+            for (Element e : pages){
+                if (e.text().equals("下一页")){
+                    userPageUrl = e.attr("href");
+                }
+            }
+        } while (userPageUrl != null);
+        System.out.print(customers.size());
     }
 
-    private static void astronVote(HttpHost host, int i) throws IOException
+    private static void compareDate(String inputDate, String createDate)
     {
-        //   Document doc = Jsoup.connect("http://www.stepon.top/misc/voicevote/index
-        // .php?from=groupmessage&isappinstalled=0").get();
-        Connection.Response res = Jsoup.connect("http://www.stepon.top/misc/voicevote/index" +
-                ".php?from=groupmessage&isappinstalled=0")
-                .method(Connection.Method.GET)
-                .execute();
-        String phpsessid = res.cookie("PHPSESSID");
-        //  System.out.println(phpsessid);
-        Document doc = res.parse();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-
-        String script = doc.select("script").get(4).toString();
-        String token = script.substring(script.lastIndexOf("'token'") + 9, script.lastIndexOf("'token'") + 49);
-
-
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost request = new HttpPost("http://www.stepon.top/misc/voicevote/api.php?op=vote");
-        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(6000).setConnectTimeout(6000).setProxy
-                (host).build();
-        request.setConfig(requestConfig);
-        StringEntity params = new StringEntity("vote-id=3025&token=" + token,
-                ContentType.APPLICATION_FORM_URLENCODED);
-        request.addHeader("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36" +
-                " (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36");
-        request.addHeader("Origin", "http://www.stepon.top");
-        request.addHeader("Host", "www.stepon.top");
-        request.addHeader("Referer", "http://www.stepon.top/misc/voicevote/index" +
-                ".php?from=groupmessage&isappinstalled=0");
-        request.addHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-        request.addHeader("Accept", "application/json, text/javascript, */*; q=0.01");
-        request.addHeader("Accept-Encoding", "gzip, deflate");
-        request.addHeader("Accept-Language", "zh-CN,zh;q=0.8");
-        request.addHeader("Connection", "keep-alive");
-        request.addHeader("Cookie", "PHPSESSID=" + phpsessid);
-        request.addHeader("X-Requested-With", "XMLHttpRequest");
-        request.setEntity(params);
-
-        String fakeForwardIp = "10.132." + (int) (Math.random() * 150 + 1) + "." + (int) (Math.random() * 150 + 1);
-        request.addHeader("X-Forwarded-For", fakeForwardIp);
-        System.out.println(fakeForwardIp);
-
-        try
-        {
-            HttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-            String responseString = EntityUtils.toString(entity, "UTF-8");
-            if (responseString.substring(8, 9).equals("1") && !responseString.substring(8, 10).equals("11"))
-            {
-                count++;
-                System.out.println("success: " + count + "; Response: " + responseString);
-                astronVote(host, count + 50);
-            }
-            else
-            {
-                System.out.println(responseString);
-            }
-        }
-        catch (Exception e)
-        {
-            //   System.out.println(e.getLocalizedMessage());
-        }
     }
 
 
